e248d6ffb1d2e883e9020fe2da2b2a0f5c68016b
KaylaMLe <66714217+KaylaMLe@users.noreply.github.com>
2021-10-30 08:49:10 -0700

    Centered and labelled axes

--- ManimAnimations/SphericalCoordinateSystem/README.md
+++ ManimAnimations/SphericalCoordinateSystem/README.md
@@ -2,5 +2,7 @@
 Show the different components of the spherical coordinate system and typical labelling.
 
 ## Release History
-* 0.0.1 (October 29, 2021)
+* 0.1 (October 29, 2021)
     * Added axes in 2D with animation
+* 0.2 (October 30, 2021)
+	* Adjusted axes to center on the origing and added rectangular labels
--- ManimAnimations/SphericalCoordinateSystem/SphericalCoordinateSystem.py
+++ ManimAnimations/SphericalCoordinateSystem/SphericalCoordinateSystem.py
@@ -1,25 +1,33 @@
 # TODO:
-# adjust axes and stylize
 # add spherical coordinate labels (mathematical)
+# add vector using spherical coordinates
 # create copy with physics labels conventions
 from manim import *
 
 
 class SphericalCoordinateSystem(Scene):
     def construct(self):
-        # creates z axis from the bottom of the screen up
-        origin_Z = Dot([0, -3, 0])
-        end_Z = Dot([0, 3, 0])
-        axis_Z = Arrow(origin_Z.get_center(), end_Z.get_center())
-        # creates y axis from the bottom left of the screen to the upper right
-        origin_Y = Dot([-3, -3, 0])
-        end_Y = Dot([3, 3, 0])
-        axis_Y = Arrow(origin_Y.get_center(), end_Y.get_center())
+        origin = Dot([0, 0, 0])
         # creates x axis from the upper left of the screen to the bottom right
-        origin_X = Dot([-3, 3, 0])
-        end_X = Dot([3, -3, 0])
-        axis_X = Arrow(origin_X.get_center(), end_X.get_center())
+        end_X = Dot([-3, -3, 0])
+        axis_X = Arrow(origin.get_center(), end_X.get_center(), buff=0)
+        # creates y axis from the bottom left of the screen to the upper right
+        end_Y = Dot([3, -3, 0])
+        axis_Y = Arrow(origin.get_center(), end_Y.get_center(), buff=0)
+        # creates z axis from the bottom of the screen up
+        end_Z = Dot([0, 3.1, 0])
+        axis_Z = Arrow(origin.get_center(), end_Z.get_center(), buff=0)
 
-        self.play(Create(axis_X))
-        self.play(Create(axis_Y))
-        self.play(Create(axis_Z))
+        # create and position labels for axes
+        label_X = Text("X", font_size=48)
+        label_X.next_to(end_X, LEFT)
+        label_Y = Text("Y", font_size=48)
+        label_Y.next_to(end_Y, RIGHT)
+        label_Z = Text("Z", font_size=48)
+        label_Z.next_to(end_Z, UP)
+
+        # animate axes creation and add labels
+        self.play(Create(axis_X), Create(axis_Y), Create(axis_Z))
+        self.wait(0.5)
+        self.add(label_X, label_Y, label_Z)
+        self.wait(0.5)
