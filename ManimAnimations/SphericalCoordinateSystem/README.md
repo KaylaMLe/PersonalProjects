9a5e080a7bb8f80a631e94f11ad8373d2c1aceb0
KaylaMLe <66714217+KaylaMLe@users.noreply.github.com>
2021-11-09 03:48:24 -0800

    Accidentally pushed subfolder to main

--- ManimAnimations/SphericalCoordinateSystem/README.md
+++ ManimAnimations/SphericalCoordinateSystem/README.md
@@ -6,3 +6,5 @@ Show the different components of the spherical coordinate system and typical lab
     * Added axes in 2D with animation
 * 0.2 (October 30, 2021)
 	* Adjusted axes to center on the origing and added rectangular labels
+* 0.3 (November 9, 2021)
+	* Added a magnitude vector and adjusted all mobjects' depths
--- ManimAnimations/SphericalCoordinateSystem/SphericalCoordinateSystem.py
+++ ManimAnimations/SphericalCoordinateSystem/SphericalCoordinateSystem.py
@@ -1,6 +1,7 @@
 # TODO:
-# add spherical coordinate labels (mathematical)
-# add vector using spherical coordinates
+# deal with color warning? (line 18 but not 15?)
+# animate r vector moving as theta and phi coordinates are altered
+# animate theta and phi arcs
 # create copy with physics labels conventions
 from manim import *
 
@@ -8,15 +9,30 @@ from manim import *
 class SphericalCoordinateSystem(Scene):
     def construct(self):
         origin = Dot([0, 0, 0])
+
+        # creates vector r to display spherical coordinate components
+        end_R = Dot([-2.5, -2.5, 0])
+        vector_R = Arrow(origin.get_center(), end_R.get_center(), color=RED, buff=0)
+        vector_R.z_index = 1
+
+        label_R = Text("R", color=RED, font_size=48)
+        label_R.next_to(vector_R, LEFT)
+        self.play(Create(vector_R))
+        self.wait(0.5)
+        self.add(label_R)
+
         # creates x axis from the upper left of the screen to the bottom right
         end_X = Dot([-3, -3, 0])
         axis_X = Arrow(origin.get_center(), end_X.get_center(), buff=0)
+        axis_X.z_index = 0
         # creates y axis from the bottom left of the screen to the upper right
         end_Y = Dot([3, -3, 0])
         axis_Y = Arrow(origin.get_center(), end_Y.get_center(), buff=0)
+        axis_Y.z_index = 0
         # creates z axis from the bottom of the screen up
         end_Z = Dot([0, 3.1, 0])
         axis_Z = Arrow(origin.get_center(), end_Z.get_center(), buff=0)
+        axis_Z.z_index = 0
 
         # create and position labels for axes
         label_X = Text("X", font_size=48)
@@ -28,6 +44,7 @@ class SphericalCoordinateSystem(Scene):
 
         # animate axes creation and add labels
         self.play(Create(axis_X), Create(axis_Y), Create(axis_Z))
-        self.wait(0.5)
+        self.bring_to_front(vector_R)
+        self.wait(1)
         self.add(label_X, label_Y, label_Z)
         self.wait(0.5)
