fc19535683ce3cad65d5576789d7573b8a395495
Kayla Marie Le <kaylale2014@gmail.com>
2022-08-26 15:29:12 -0700

    changed some formatting settings, deleted janky code, and started on a complete re-implementation

--- Simpletron/README.md
+++ Simpletron/README.md
@@ -4,33 +4,27 @@
 * Language: C++
 
 ## Installation
-* The pre-compiled executable is compatible with Windows.*
-With GCC on Windows, Mac OS X, and Unix systems:
+With GCC on Windows and Unix systems:
 
 ```sh
 g++ Simpletron.cpp exe_overview.cpp -o Simpletron
 ```
 
-## Usage example
-
-![Entering words into Simpletron](https://i.imgur.com/EwnH9Si.png)
-![Simpletron execution and memory dump](https://i.imgur.com/ViNrCtn.png)
-
-Enter operation codes (see ``OperationCodes`` for a full list) and target addresses into the terminal to initialize a program. After execution, all register and memory values are displayed.
-
 ## Release History
 *Status: In progress*
 * 1.0 (October 26, 2021)
-    * First version: basic program structure and functions present
+   * First version: basic program structure and functions present
 * 1.1 (October 31, 2021)
-    * Separated main file into Simpletron.cpp and appropriate header files
+  * Separated main file into Simpletron.cpp and appropriate header files
 * 1.2 (November 10, 2021)
-	* Added CPSR flags and checks them to branch. Also added branchoverflow
+  * Added CPSR flags and checks them to branch. Also added branchoverflow
 * 1.21 (January 8, 2022)
-	* Hotfix: Latest version now compiles without error
+  * Hotfix: Latest version now compiles without error
 * 1.22 (January 11, 2022)
-	* Additional bug fixes: Words are now properly stored without causing an early memdump.
+  * Additional bug fixes: Words are now properly stored without causing an early memdump.
 * 1.3 (January 16, 2022)
-	* Overflows trigger an alert without ending execution. Flags are properly updated.
+  * Overflows trigger an alert without ending execution. Flags are properly updated.
 * 1.4 (January 17, 2022)
-	* End of execution memdump now also written into a file
+  * End of execution memdump now also written into a file
+* 2.0 (August 26, 2022)
+  * Started some major fixes to better align with the project's goal and use better software engineering practices
--- Simpletron/Simpletron.cpp
+++ Simpletron/Simpletron.cpp
@@ -1,55 +1,22 @@
-// TODO:
-// overflow but not crash
-// implement carry flag
-// optimize opcode checking during execution (impractical and slow to check an entire case/switch of every code)
-// rework to register 0 - 16
-// use string-based words (enumerate?)
-// enable reading/writing to files (.s or .txt?)
-
-#include "exe_overview.h"
+#include <fstream>
 #include <iostream>
-#include <iomanip>
-#include <array>
-using namespace runtime;
-
-const size_t MEM_SIZE{ 100 };
-processes prcs;
+using namespace std;
 
 int main()
 {
-    int memory[MEM_SIZE]{ 0 };
+    // asks the user to specify a file to run
+    ifstream sourceFile;
+    string sourceFilePath;
 
-    // registers
-    int accumulator{ 0 }, instructionRegister{ 0 }, PC{ 0 }, operationCode{ 0 }, operand{ 0 };
-
-    std::cout << "\a*** Welcome to Simpletron! ***\n"// note the \a (alarm)
-        << "*** Please enter your program one instruction ***\n"
-        << "*** (or data word) at a time. I will type the ***\n"
-        << "*** location number and a question mark (?). ***\n"
-        << "*** You then type the word for that location. ***\n"
-        << "*** Type the sentinel -99999 to stop entering ***\n"
-        << "*** your program. ***\n" << std::endl;
-
-    std::cout << std::setfill('0');
-
-    for (int i{ 0 }; i < MEM_SIZE; i++) {
-        do {
-            // adds leading zero to digits
-            std::cout << std::setw(2);
-            std::cout << i;
-            std::cout << std::setw(0);
-            std::cout << " ? ";
-
-            std::cin.clear();// allows iteration
-            std::cin >> memory[i];
-        } while ((memory[i] < -9999 || memory[i] > +9999) && memory[i] != -99999);// if not a four digit integer, re-prompt
-
-        if (memory[i] == -99999) {
-            break;
-        }
+    while (!(sourceFile.is_open()))
+    {
+        cout << "Path to source file: ";
+        cin >> sourceFilePath;
+        sourceFile.open(sourceFilePath, std::fstream::in | std::fstream::out);
+        cin.clear();
     }
 
-    std::cout << "\n*** Program loading completed ***\n" << "*** Program execution begins ***\n" << std::endl;
-    prcs.execute(memory, accumulator, PC, instructionRegister, operationCode, operand);
+    cout << "file opened";
+
     return 0;
 }
