ab4e3a62b6f4dbebdbe84d0f441f223e06024be3
KaylaMLe <kaylale2014@gmail.com>
2023-08-20 16:22:32 -0700

    worked out grid to align dog parts

--- Dog/web-dog/src/dog.js
+++ Dog/web-dog/src/dog.js
@@ -1,19 +1,30 @@
-import body from "./assets/body.png";
+import head from "./assets/head.png";
+import leg from "./assets/leg.png";
 import tail from "./assets/tail.png";
+import body from "./assets/body.png";
 
 export function Dog() {
   return (
     <div className="Dog">
+      <Head />
+      <Leg />
       <Body />
-      <Tail />
     </div>
   );
 }
 
-function Body() {
-  return <img className="Body" src={body} alt="the dog's fluffy body" />;
+function Head() {
+  return <img className="Head" src={head} alt="the dog's head" />;
+}
+
+function Leg() {
+  return <img className="Leg" src={leg} alt="one of the dog's legs" />;
 }
 
 function Tail() {
   return <img className="Tail" src={tail} alt="a waggly tail" />;
+}
+
+function Body() {
+  return <img className="Body" src={body} alt="the dog's fluffy body" />;
 }
